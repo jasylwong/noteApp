be6a98fb29c956b16b4d2fdc04b170082962c184
Jason <jasylwong@gmail.com>
2020-03-04 23:00:13 +0000

    ch9: give notes unique ids

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -8,4 +8,13 @@ describe('Note', () => {
       expect(note.showText()).toBe(testingText)
     })
   })
+
+  describe('.id', () => {
+    it('assigns a unique id to each note, based on creation order', () => {
+      var note1 = new Note('Note uno');
+      var note2 = new Note('Note dos');
+      expect(note1.id).toBe(1); //will be 1 because instantiated a note object in above test already, which has id 0       
+      expect(note2.id).toBe(2);
+    })
+  })
 })
